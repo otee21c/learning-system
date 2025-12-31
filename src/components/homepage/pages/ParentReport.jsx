4c1abe7a33ab553e4a0d8c4d5677db241cd6d03c
otee21c <otee21c@gmail.com>
2025-12-31 17:54:46 +0900

    firebase 경로 수정

--- src/components/homepage/pages/ParentReport.jsx
+++ src/components/homepage/pages/ParentReport.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { collection, getDocs, query, where } from 'firebase/firestore';
-import { db } from '../../firebase';
+import { db } from '../../../firebase';
 import { 
   User, Phone, Search, TrendingUp, BookOpen, CheckCircle, 
   BarChart3, Calendar, ChevronDown, ChevronUp, FileText,
